72bbac1169c8c8bc12998e696fff018848d66380
rover0811 <rover0811@hotmail.com>
2022-09-07 16:07:08 +0900

    Add ListItem Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import "react-native-gesture-handler";
@@ -7,6 +7,7 @@ import * as React from "react";
 import CustomDrawer from "./CustomDrawer";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { ListItem } from "@rneui/themed";
 
 const Drawer = createDrawerNavigator();
 const Tab = createBottomTabNavigator();
@@ -39,9 +40,9 @@ function DrawerComponent() {
           ),
         }}
       />
-      <Drawer.Screen
+      {/* <Drawer.Screen
         name="Phone"
-        component={Phone}
+        component={UnderTab}
         options={{
           drawerIcon: ({ color }) => (
             <MaterialCommunityIcons name="phone" size={30} color={color} />
@@ -56,7 +57,8 @@ function DrawerComponent() {
             <MaterialCommunityIcons name="forum" size={30} color={color} />
           ),
         }}
-      />
+      /> */}
+      {/* 여기서 navigate해서 탭바의 정확한 그곳으로 가게끔 할 수가 있나?!*/}
       <Drawer.Screen
         name="Setting"
         component={SettingScreen}
@@ -96,21 +98,107 @@ function Home() {
     </SafeAreaView>
   );
 }
+
+const list = [
+  {
+    name: "홍길동",
+    subtitle: "010-5029-9071",
+  },
+  {
+    name: "김기열",
+    subtitle: "Vice Chairman",
+  },
+  {
+    name: "김준현",
+    subtitle: "Vice Chairman",
+  },
+  {
+    name: "강유미",
+    subtitle: "Vice Chairman",
+  },
+  {
+    name: "유민상",
+    subtitle: "Vice Chairman",
+  },
+  {
+    name: "문세윤",
+    subtitle: "Vice Chairman",
+  },
+  {
+    name: "김민경",
+    subtitle: "Vice Chairman",
+  },
+  {
+    name: "허경환",
+    subtitle: "Vice Chairman",
+  },
+  {
+    name: "Chris Jackson",
+    subtitle: "Vice Chairman",
+  },
+  {
+    name: "Chris Jackson",
+    subtitle: "Vice Chairman",
+  },
+  {
+    name: "Chris Jackson",
+    subtitle: "Vice Chairman",
+  },
+  {
+    name: "Chris Jackson",
+    subtitle: "Vice Chairman",
+  },
+  {
+    name: "Chris Jackson",
+    subtitle: "Vice Chairman",
+  },
+  {
+    name: "Chris Jackson",
+    subtitle: "Vice Chairman",
+  },
+  {
+    name: "Chris Jackson",
+    subtitle: "Vice Chairman",
+  },
+  {
+    name: "Chris Jackson",
+    subtitle: "Vice Chairman",
+  },
+];
+
 function Phone() {
   return (
     <SafeAreaView
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    // style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
     >
-      <Text>Phone</Text>
+      <ScrollView>
+        {list.map((l, i) => (
+          <ListItem key={i} bottomDivider>
+            <ListItem.Content>
+              <ListItem.Title>{l.name}</ListItem.Title>
+              <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        ))}
+      </ScrollView>
     </SafeAreaView>
   );
 }
 function Messages() {
   return (
     <SafeAreaView
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    // style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
     >
-      <Text>Messages</Text>
+      <ScrollView>
+        {list.map((l, i) => (
+          <ListItem key={i} bottomDivider>
+            <ListItem.Content>
+              <ListItem.Title>{l.name}</ListItem.Title>
+              <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        ))}
+      </ScrollView>
     </SafeAreaView>
   );
 }
